8169a4201299483b5bf2ce16df6fbc81121a7588
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-02-05 01:04:21 +0100

    so now as we can see i have printed the actual y_test and the predicted price value (predic) and i also have  a visualized graph to show the my model

--- house_price_linear_regression.py
+++ house_price_linear_regression.py
@@ -7,10 +7,9 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('house_price.csv')
-x= dataset.iloc[:, :-1].givalues
+x= dataset.iloc[:, :-1].values
 y= dataset.iloc[:, -1].values
-print(x)
-print(y)
+
 
 #split the data set 
 from sklearn.model_selection import train_test_split
@@ -24,12 +23,31 @@ regressor.fit(x_train,y_train)
 #the prediction result
 predic=regressor.predict(x_test)
 
+#Displaying predictions and actual values
+result_df= pd.DataFrame( { 'Actual' : y_test,'Predicted': predic}) 
+print(result_df)
 
 #visualization
 
 # for the training set
 plt.scatter(x_train, y_train, color='red')
 plt.plot(x_train, regressor.predict(x_train), color='blue')
-plt.title('Salary vs Experience (Training set)')
+plt.title('square foot area vs price(Training set)')
 plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.ylabel('price')
+
+#plt.show()
+
+
+#for the test set
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_test, regressor.predict(x_test), color='blue')  
+plt.title('square foot area vs price(Test set)')
+plt.xlabel('square foot area ')
+plt.ylabel('price')
+
+# Display the second plot
+plt.show()
+
+# Displaying predictions and actual values
+result_df= pd.DataFrame( { 'Actual' : y_test,'Predicted': predic}) 
